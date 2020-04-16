61b3b691ff313a75d06130e8932f0deb91dfd081
Adam Ponting <adam.ponting@gmail.com>
2020-04-16 16:12:21 +0100

    Completed #09

--- playground/types.js
+++ playground/types.js
@@ -1,86 +1,81 @@
 /* eslint-disable */
 
-const name = 'wes';
-const middle = 'topher';
-const last = `bos`;
+// const name = 'wes';
+// const middle = 'topher';
+// const last = `bos`;
 
-const sentence = 'she\'s so "cool"';
-const sentence2 = `she's so \"cool\"`;
+// const sentence = 'she\'s so "cool"';
+// const sentence2 = `she's so \"cool\"`;
 
-const song = `Ohhh
+// const song = `Ohhh
 
-ya
+// ya
 
-I like
-pizza`;
+// I like
+// pizza`;
 
-// const hello = 'hello my name is ' + name + '. Nice to meet you';
+// // const hello = 'hello my name is ' + name + '. Nice to meet you';
 
-let hello2 = 'hello my name is ';
-hello2 = hello2 + name;
-hello2 = hello2 + '. Nice to meet you';
+// let hello2 = 'hello my name is ';
+// hello2 = hello2 + name;
+// hello2 = hello2 + '. Nice to meet you';
 
-const hello = `hello my name is ${name}. Nice to meet you. I am ${1 + 100} years old`;
+// const hello = `hello my name is ${name}. Nice to meet you. I am ${1 + 100} years old`;
 
-const html = `
-<div>
-  <div>
-    <h2>${name}</h2>
-    <p>${hello}</p>
-  </div>
-`;
+// const html = `
+// <div>
+//   <div>
+//     <h2>${name}</h2>
+//     <p>${hello}</p>
+//   </div>
+// `;
 
-document.body.innerHTML = html;
+// document.body.innerHTML = html;
 
 // const age = 100.5;
 // const name = 'wes';
 
 // const a = 10;
 // const b = 20;
-/*
-const smarties = 20;
-const kids = 3;
-const eachKidGets = Math.floor(smarties / kids);
-const dadGets = smarties % kids;
-console.log(`Each kids gets ${eachKidGets}`);
 
-const price = 1034;
-*/
+// const smarties = 20;
+// const kids = 3;
+// const eachKidGets = Math.floor(smarties / kids);
+// const dadGets = smarties % kids;
+// console.log(`Each kids gets ${eachKidGets}`);
 
-/*
+// const price = 1034;
+
 const person = {
   first: 'wes',
   last: 'bos',
-  age: 100
+  age: 100,
 };
+console.log(person);
 
+// let dog;
+// console.log(dog);
+// dog = 'snickers';
 
-let dog;
-console.log(dog);
-dog = 'snickers';
+// let somethingUndefined;
+// const somethingNull = null;
 
+// const cher = {
+//   first: 'cher',
+// };
 
-let somethingUndefined;
-const somethingNull = null;
+// const teller = {
+//   first: 'Raymond',
+//   last: 'Teller',
+// };
 
-const cher = {
-  first: 'cher'
-};
+// teller.first = 'Teller';
+// teller.last = null;
 
-const teller = {
-  first: 'Raymond',
-  last: 'Teller'
-}
+// let isDrawing = false;
+// let age = 18;
+// const ofAge = age > 19;
+// console.log(ofAge);
 
-teller.first = 'Teller';
-teller.last = null;
-
-*/
-
-let isDrawing = false;
-let age = 18;
-const ofAge = age > 19;
-console.log(ofAge);
-
-age = 100;
-let age2 = 100;
+// age = 100;
+// let age2 = 100;
